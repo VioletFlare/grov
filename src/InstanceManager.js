bff2411970ed56a5f20c5b7c6d04f68b6a5c4624
violetflare@protonmail.com <violetflare@protonmail.com>
2021-10-27 22:00:38 +0200

    init bot on guild join

--- src/InstanceManager.js
+++ src/InstanceManager.js
@@ -37,33 +37,45 @@ class InstanceManager {
         }
     }
 
+    _initSession(guild) {
+        this.sessions.set(guild.id, new Grov(guild));
+    }
+
     _setActivity() {
         this.client.user.setActivity(
             `gr/help`, {type: 'PLAYING'}
         );
     }
 
-    init() {
-        if (this.isDev) {
-            this.client.login(config.TOKEN_DEV);
-          } else {
-            this.client.login(config.TOKEN_PROD);
-          }
-
-          this.client.on("ready", () => {
+    _setEvents() {
+        this.client.on("ready", () => {
             console.log(`Logged in as ${this.client.user.tag}, id ${this.client.user.id}!`);
 
             this._setActivity();
             this._initSessions();
           });
           
-          this.client.on(
+        this.client.on(
             "voiceStateUpdate", (oldState, newState) => this._onVoiceStateUpdate(oldState, newState)
-          );
+        );
           
-          this.client.on(
-              "message", msg => this._onMessage(msg)
-          );
+        this.client.on(
+            "message", msg => this._onMessage(msg)
+        );
+
+        this.client.on(
+            "guildCreate", guild => this._initSession(guild)
+        );
+    }
+
+    init() {
+        if (this.isDev) {
+            this.client.login(config.TOKEN_DEV);
+        } else {
+            this.client.login(config.TOKEN_PROD);
+        }
+
+        this._setEvents();
     }
 
 }
